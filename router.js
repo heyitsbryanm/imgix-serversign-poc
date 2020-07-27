5aedcd8c3121b7b32ab8aa52acfb1f92fa23d35f
heyitsbryanm <matiasbryan@gmail.com>
2020-07-27 16:38:07 -0700

    Migrates project to codesandbox

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 
-const sign_images = require('./routes/sign_images');
-router.use('/sign_images',sign_images)
+const sign_images = require("./routes/sign_images");
+router.use("/sign_images", sign_images);
 
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/html/index.html"));
+});
 module.exports = router;
-
